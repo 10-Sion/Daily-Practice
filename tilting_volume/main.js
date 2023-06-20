5615463cf6da840984f9acabdec9828ce58b6cef
10-Sion <66767165+10-Sion@users.noreply.github.com>
2023-06-20 12:46:19 +0900

    Update main.js

--- tilting_volume/main.js
+++ tilting_volume/main.js
@@ -9,26 +9,41 @@ $('.tiltProg').on('mousedown', () => {
   $(window).on('mousemove', (e) => { 
     if(isProgClicking) {
       const currentY = e.pageY - $('.tiltProg').offset().top;
-      console.log(currentY);
-      tiltStart(currentY);
+      console.log(currentY);    //  Y값 확인용
+      tiltStart(currentY);      //  회전용 함수
+      progressMovin(currentY);  //  그래프 함수
+
+
     }
   });
 
-  interval = setInterval(() => {
+  $(window).on('mouseup', () => {
+    isProgClicking = false;
+    clearInterval(interval);
 
-  });
-
-  
+    $('.tiltProg').css('transform', 'rotate(0deg)')
+    $('*').css('transition','transform .5s ease')
+  })
 });
 
 
 
-
   function tiltStart(y) {
-    const tiltAmount = (y / $('.tiltProg').height()) * 360;
-    $('.tiltProg').css('transform', `rotate(${tiltAmount}deg)`);
+    // 최대 각도의 설정
+    const tiltAmount = (y / $('.tiltProg').height()) * 90;
+    // 각도 변경값의 둔화  
+    $('.tiltProg').css('transform', `rotate(${tiltAmount/45}deg)`);
   }
 
   function tiltDone() {
     $('.tiltProg').css('transform', 'rotate(0deg)');
+  }
+
+  //  현재 볼륨값 텍스트
+    function progressMovin(y) {
+    let valueTxt = Math.floor(y / 2)
+    const currentVal = valueTxt < 0 ? 0 : (valueTxt > 100 ? 200 : y);
+
+    $('.tiltProg').attr('value',y/2)
+    $('#currentVolume').text(currentVal/2)
   }
